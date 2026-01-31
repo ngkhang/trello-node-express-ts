9f43f76f0a6f2e1c1bd28d9a6d43fa0d50f264d8
Khang Nguyen <ngkhang0220@gmail.com>
2026-01-31 15:47:54 +0700

    feat(middleware): integrate `morgan`, `helmet`, and `cookie-parser`
    - `morgan` for HTTP request logging
    - `helmet` to secure HTTP response headers
    - `cookie-parser` for parsing cookie headers

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@
  * Last Updated: 2026-01-31
  ------------------------------------------------- */
 
+import cookieParser from 'cookie-parser';
 import type { Express } from 'express';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import routes from './routes';
 
@@ -19,6 +22,12 @@ export const createApp = (): Express => {
   app.use(express.json());
   // CORS
   app.use(corsMiddleware);
+  // HTTP request logger middleware
+  app.use(morgan('dev'));
+  // Secure Express apps by setting HTTP response headers
+  app.use(helmet());
+  // Parse Cookie header and populate req.cookies
+  app.use(cookieParser());
 
   // API Routes
   app.use('/api', routes);
